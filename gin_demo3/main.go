5295ea5a610a20860679abfbc59088f3301e4ba1
liuzy <liuzy@fun.tv>
2021-06-25 18:37:24 +0800

    Next方法

--- gin_demo3/main.go
+++ gin_demo3/main.go
@@ -14,6 +14,9 @@ func MiddleWare() gin.HandlerFunc {
 		fmt.Println("中间件开始执行了")
 		// 设置变量到Context的key中，可以通过Get()取
 		c.Set("request", "中间件")
+		// 执行函数
+		c.Next()
+		// 中间件执行完后续的一些事情
 		status := c.Writer.Status()
 		fmt.Println("中间件执行完毕", status)
 		t2 := time.Since(t)
